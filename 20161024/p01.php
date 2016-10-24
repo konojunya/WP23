2600a3e5251eff10e15d60551af27498c5894443
konojunya <junya03dance@gmail.com>
2016-10-24 15:02:22 +0900

    2016年 10月24日 月曜日 15時02分22秒 JST

---
+++ 20161024/p01.php
@@ -0,0 +1,26 @@
+<?php
+//******************************
+//
+//	クラス記号: IH-12A-621
+//  名前: 河野純也
+//  日付: 2016/10/24
+//
+//******************************
+
+$class_room = $_GET['class_room'];
+$room_class = $_GET['room_number'];
+$name = $_GET['name'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<p>クラス: <?= $class_room ?></p>
+	<p>出席: <?= $room_class ?></p>
+	<p>氏名: <?= $name ?></p>
+</body>
+</html>
